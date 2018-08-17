58d6b2ee75d31567ac8d9a499d427238b4aebce0
Alison Fahy <fahyalison@gmail.com>
2018-08-16 23:35:32 -0500

    push

---
+++ nytimesreact/src/utils/API.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+const BASEURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=";
+const APIKEY = "309dee416d1b4b9994cb6429008f7260";
+
+export default {
+  search: function(query) {
+    return axios.get(BASEURL + APIKEY + query);
+  },
+  saveArticle: function(articleObject){
+    return axios.post('/saveArticle', articleObject);
+  },
+  getSavedArticles: function(){
+    return axios.get('/getSavedArticles');
+  },
+  deleteSavedArticle: function(id){
+    return axios.delete('/deleteSavedArticle', {data: {articleId: id}})
+  }
+
+};
